7a9250fe1fdd4c630f1fc79370a148ff3ffcbe27
brianlizhengyu@gmail.com <brianlizhengyu@gmail.com>
2023-12-19 21:59:00 -0500

    script for cc, need testing

---
+++ 3-cube-merge-solve-iterative-cc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+n=$1 #order
+f=$2 #instance file name
+d=$3 #directory to store into
+v=$4 #num of var to eliminate during first cubing stage
+a=$5 #amount of additional variables to remove for each cubing call
+
+#we want the script to: cube, for each cube, submit sbatch to solve, if not solved, call the script again
+
+mkdir -p $d/$v
+
+command="python -u alpha-zero-general/main.py $f -n $v -m $m -o $dir/$v.cubes -order $n -numMCTSSims 30 -prod | tee $dir/$v.log"
+echo $command
+eval $command
+
+cube_file=$dir/$v.cubes
+
+new_index=$(< $cube_file wc -l)
+
+for i in $(seq 1 $new_index) #1-based indexing for cubes
+    do
+        command1="./gen_cubes/apply.sh $f $cube_file $i > $cube_file$i.adj"
+        command2="./solve-verify.sh $n $cube_file$i.adj"
+        child_instance="$cube_file$i.adj"
+        file="$cube_file$i.adj.log"
+        command3="if ! grep -q 'UNSATISFIABLE' '$file'; then sbatch $child_instance-cube.sh; fi"
+        #sbatch this line
+        command4="./3-cube-merge-solve-iterative-cc.sh $n $child_instance '$d/$v-$i' $(($v + $a)) $a"
+        command="$command1 && $command2"
+        echo "#!/bin/bash" > $child_instance-solve.sh
+        echo "#SBATCH --account=rrg-cbright" >> $child_instance-solve.sh
+        echo "#SBATCH --time=2-00:00" >> $child_instance-solve.sh
+        echo "#SBATCH --mem-per-cpu=4G" >> $child_instance-solve.sh
+        echo "#!/bin/bash" > $child_instance-cube.sh
+        echo "#SBATCH --account=rrg-cbright" >> $child_instance-cube.sh
+        echo "#SBATCH --time=2-00:00" >> $child_instance-cube.sh
+        echo "#SBATCH --mem-per-cpu=4G" >> $child_instance-cube.sh
+        echo $command >> $child_instance-solve.sh
+        echo $command3 >> $child_instance-solve.sh
+        echo $command4 >> $child_instance-cube.sh
+        sbatch $child_instance-solve.sh
+    done
--- main-cc.sh
+++ main-cc.sh
@@ -1,25 +1,59 @@
+#!/bin/bash
+
+# Ensure parameters are specified on the command-line
+
+[ "$1" = "-h" -o "$1" = "--help" ] && echo "
+Description:
+    Updated on 2023-12-19
+    This is a driver script that handles generating the SAT encoding, solve the instance using CaDiCaL, then finally determine if a KS system exists for a certain order.
+
+Usage:
+    ./main.sh [-p] n r a
+    If only parameter n is provided, default run ./main.sh n 0 0
+
+Options:
+    [-p]: cubing/solving in parallel
+    <n>: the order of the instance/number of vertices in the graph
+    <r>: number of variable to remove in cubing, if not passed in, assuming no cubing needed
+    <a>: amount of additional variables to remove for each cubing call
+" && exit
+
+while getopts "pm" opt
+do
+    case $opt in
+        p) p="-p" ;;
+        m) m="-m" ;;
+        *) echo "Invalid option: -$OPTARG. Only -p and -m are supported. Use -h or --help for help" >&2
+           exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+
+#step 1: input parameters
+if [ -z "$1" ]
+then
+    echo "Need instance order (number of vertices), use -h or --help for further instruction"
+    exit
+fi
+
 n=$1 #order
-t=${2:-10000} #conflicts for which to simplify each time CaDiCal is called, or % of variables to eliminate
-s=${3:-2} #by default we only simplify the instance using CaDiCaL after adding noncanonical blocking clauses
-b=${4:-2} #by default we generate noncanonical blocking clauses in real time
-r=${5:-0} #num of var to eliminate during first cubing stage
-a=${6:-10} #amount of additional variables to remove for each cubing call
-z=${7:-1} #cubing strategy 1: march 2: ams with no mcts 3: ams with mcts
+r=${2:-0} #num of var to eliminate during first cubing stage
+a=${3:-0} #amount of additional variables to remove for each cubing call
 
 #step 2: setp up dependencies
 ./dependency-setup.sh
-
+ 
 #step 3 and 4: generate pre-processed instance
 
 dir="."
 
-if [ -f constraints_${n}_${t}_${s}_${b}_${r}_${a}_final.simp.log ]
+if [ -f constraints_${n}_${r}_${a}_final.simp.log ]
 then
     echo "Instance with these parameters has already been solved."
     exit 0
 fi
 
-./generate-simp-instance.sh $n $t $s $b $r $a
+./1-instance-generation.sh $n
 
 if [ -f "$n.exhaust" ]
 then
@@ -33,29 +67,10 @@ fi
 
 #need to fix the cubing part for directory pointer
 #step 5: cube and conquer if necessary, then solve
-if [ "$r" != "0" ]
+if [ "$r" != "0" ] 
 then
-    dir="${n}_${t}_${s}_${b}_${r}_${a}_${z}"
-    #./3-cube-merge-solve-ams.sh constraints_${n}_${t}_${s}_${b}_${r}_${a}_final.simp $n $r $dir
-    echo "cubing with option $z"
-    ./3-cube-merge-solve-iterative-learnt-cc.sh $p $n constraints_${n}_${t}_${s}_${b}_${r}_${a}_final.simp $dir $r $t $a $z
-    command="./summary-iterative.sh $dir $r $a $n"
-    echo $command
-    eval $command
-    #join all exhaust file together and check embeddability
-    find "$dir" -type f -name "*.exhaust" -exec cat {} + > "$dir/$n.exhaust"
-    ./verify.sh $dir/$n.exhaust $n
-    ./4-check-embedability.sh $n $dir/$n.exhaust
+    dir="${n}_${r}_${a}"
+    ./3-cube-merge-solve-iterative-cc.sh $p $n constraints_${n} $dir $r $a
 else
-    ./solve-verify.sh $n constraints_${n}_${t}_${s}_${b}_${r}_${a}_final.simp $n.exhaust
-    #step 5.5: verify all constraints are satisfied
-    ./verify.sh $n.exhaust $n
-
-    #step 6: checking if there exist embeddable solution
-    echo "checking embeddability of KS candidates using Z3..."
-    ./4-check-embedability.sh $n $n.exhaust
-
-    #output the number of KS system if there is any
-    echo "$(wc -l < $n.exhaust) Kochen-Specker candidates were found."
-    echo "$(wc -l < $n.exhaust-embeddable.txt) Kochen-Specker solutions were found."
+    ./solve-verify.sh $n constraints_${n}
 fi
