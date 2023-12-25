0c75cb40cf004687f7fedea45773744e10386405
brianlizhengyu@gmail.com <brianlizhengyu@gmail.com>
2023-12-25 00:16:36 -0500

    add noncanonical blocking clauses

--- 2-add-blocking-clauses.sh
+++ 2-add-blocking-clauses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ "$1" = "-h" -o "$1" = "--help" -o "$#" -ne 3 ] && echo "
+[ "$1" = "-h" -o "$1" = "--help" -o "$#" -ne 2 ] && echo "
 Description:
     Updated on 2023-01-11
     This script generate non-canonical blocking clauses of order o using maplesat-ks, then concanate the clauses into the instance.
@@ -10,17 +10,18 @@ Usage:
 
 Options:
     <n>: the order of the instance/number of vertices in the graph
-    <o>: the size of the noncanonical subgraph we want to generate and block
     <f>: file name of the current SAT instance
 " && exit
 
 n=$1 #order of the graph we are solving
-o=$2 #subgraph's order
-f=$3 #instance file name
+f=$2 #instance file name
 #generate non canonical subgraph
-./run-subgraph-generation.sh $n $f $o
 
-#concanate all blocking clauses and add them to the file
-cat $n/$o-solo.noncanonical >> $f
+command="./cadical-ks/build/cadical-ks $f $f.drat --order $n --unembeddable-check 17 --no-binary -c 10000"
+
+echo $command
+eval $command
+
+grep 't' $f.drat | cut -d' ' -f 2- >> $f
 lines=$(wc -l < "$f")
 sed -i -E "s/p cnf ([0-9]*) ([0-9]*)/p cnf \1 $((lines-1))/" "$f"
