3228702e8dea0452311258a7035e95bd2631cf76
brianlizhengyu@gmail.com <brianlizhengyu@gmail.com>
2024-01-23 09:48:14 -0500

    ready

--- prepare-parallel-solve.sh
+++ prepare-parallel-solve.sh
@@ -13,3 +13,31 @@ nodes=$5 #number of nodes to submit to in total
 ./simplification/simplify.sh constraints_${n}_0.5 $n $s
 
 ./cube-solve-cc.sh $n constraints_${n}_0.5.simp $d $c1 $c2 constraints_${n}_0.simp
+
+total_lines=$(wc -l < "constraints_${n}_0.5.simp.commands")
+((lines_per_file = (total_lines + $nodes - 1) / $nodes))
+
+# Shuffle and split the file
+shuf "constraints_${n}_0.5.simp.commands" | split -l "$lines_per_file" - shuffled_
+
+counter=1
+for file in shuffled_*; do
+    mv "$file" "constraints_${n}_0.5.simp.commands${counter}"
+    cat <<EOF > "script_${counter}.sh"
+#!/bin/bash
+#SBATCH --account=rrg-cbright
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=0
+#SBATCH --time=1-00:00
+
+module load python/3.10
+python parallel-solve.py $n constraints_${n}_0.5.simp $d $c1 $c2 constraints_${n}_0.5.simp.commands${counter}
+EOF
+
+    ((counter++))
+done
+
+
+
