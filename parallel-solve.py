92473cfe9ebe017ecce20cdea82a2c97bd705843
Brian Li <brianli.li@mail.utoronto.ca>
2024-02-01 21:28:25 -0500

    still testing adding cubing to multiprocessing

--- parallel-solve.py
+++ parallel-solve.py
@@ -80,10 +80,10 @@ def worker(queue):
 def cube(file_to_cube, m, directory, order, numMCTS, logdir, queue):
     subprocess.run(f"python3 -u alpha-zero-general/main.py {file_to_cube} -d 1 -m {m} -o {directory}/{file_to_cube}.cubes -order {order} -prod -numMCTSSims {numMCTS}", shell=True)
     #for i in number of line in 
-    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {directory}/{file_to_cube}.cubes 1 > {file_to_cube}{1}.cubes")
-    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {directory}/{file_to_cube}.cubes 2 > {file_to_cube}{2}.cubes")
-    command1 = f"cube('{file_to_cube}{1}.cubes', {m}, '{directory}', '{order}', {numMCTS}, '{logdir}', queue)"
-    command2 = f"cube('{file_to_cube}{2}.cubes', {m}, '{directory}', '{order}', {numMCTS}, '{logdir}', queue)"
+    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {directory}/{file_to_cube}.cubes 1 > {directory}/{file_to_cube}{1}.cubes")
+    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {directory}/{file_to_cube}.cubes 2 > {directory}/{file_to_cube}{2}.cubes")
+    command1 = f"cube('{directory}/{file_to_cube}{1}.cubes', {m}, '{directory}', '{order}', {numMCTS}, '{logdir}', queue)"
+    command2 = f"cube('{directory}/{file_to_cube}{2}.cubes', {m}, '{directory}', '{order}', {numMCTS}, '{logdir}', queue)"
     queue.put(command1)
     queue.put(command2)
             
