6e9eb6e1adc12828b3645678f0c3afb1f76a9e01
Brian Li <brianli@Brians-MacBook-Pro.local>
2024-02-02 17:00:12 -0500

    testing

--- parallel-solve.py
+++ parallel-solve.py
@@ -103,7 +103,7 @@ def cube(file_to_cube, m, order, numMCTS, queue, cutoff='d', cutoffv=5, d=0, n=0
     queue.put(command1)
     queue.put(command2)
 
-def main(order, file_name_solve, numMCTS=2, cutoff='d', cutoffv='5', d=0, n=0):
+def main(order, file_name_solve, numMCTS=2, cutoff='d', cutoffv=5, d=0, n=0):
     global queue
     queue = multiprocessing.JoinableQueue()
     num_worker_processes = multiprocessing.cpu_count()
