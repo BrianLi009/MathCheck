4e8b64535adf702d07bdb77fbd7f8438a8fb0fd5
Brian Li <brianli@Brians-MacBook-Pro.local>
2024-02-02 14:33:02 -0500

    testing

--- parallel-solve.py
+++ parallel-solve.py
@@ -82,10 +82,10 @@ def worker(queue):
 def cube(file_to_cube, m, order, numMCTS, logdir, queue):
     subprocess.run(f"python -u alpha-zero-general/main.py {file_to_cube} -d 1 -m {m} -o {file_to_cube}.cubes -order {order} -prod -numMCTSSims {numMCTS}", shell=True)
     #for i in number of line in 
-    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {file_to_cube}.cubes 1 > {file_to_cube}{1}.cubes", shell=True)
-    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {file_to_cube}.cubes 2 > {file_to_cube}{2}.cubes", shell=True)
-    command1 = f"cube('{file_to_cube}{1}.cubes', {m}, '{order}', {numMCTS}, '{logdir}', queue)"
-    command2 = f"cube('{file_to_cube}{2}.cubes', {m}, '{order}', {numMCTS}, '{logdir}', queue)"
+    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {file_to_cube}.cubes 1 > {file_to_cube}{1}", shell=True)
+    subprocess.run(f"./gen_cubes/apply.sh {file_to_cube} {file_to_cube}.cubes 2 > {file_to_cube}{2}", shell=True)
+    command1 = f"cube('{file_to_cube}{1}', {m}, '{order}', {numMCTS}, '{logdir}', queue)"
+    command2 = f"cube('{file_to_cube}{2}', {m}, '{order}', {numMCTS}, '{logdir}', queue)"
     queue.put(command1)
     queue.put(command2)
 
