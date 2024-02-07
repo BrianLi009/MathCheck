e8f5c97ae1bf3aaa7792b100cfbfe1a1c669e932
Brian Li <brianli@unknown8620f6a5d80c.attlocal.net>
2024-02-07 15:39:00 -0500

    testing main script

--- main.sh
+++ main.sh
@@ -43,7 +43,7 @@ fi
 n=$1 # Order
 c=${2:-0.5} # Color percentage
 m=${3:-2} #Num of MCTS simulations. m=0 activate march
-d=${4:-d} #Cubing cutoff criteria, choose d(depth) as default
+d=${4:-d} #Cubing cutoff criteria, choose d(depth) as default #d, n, v
 dv=${5:-5} #By default cube to depth 5
 nodes=${6:-1} #Number of nodes to submit to if using -l
 
@@ -73,32 +73,7 @@ case $solve_mode in
         ;;
     "par_cubing")
         echo "Cubing and solving in parallel on Compute Canada"
-        python parallel-solve.py $n constraints_${n}_${c} $m False n $nodes
+        python parallel-solve.py $n constraints_${n}_${c} $m False $d $dv
         i=1
-        for file in constraints_${n}_${c}*.simp; do
-            cat <<EOF > "script_${i}.sh"
-#!/bin/bash
-#SBATCH --account=rrg-cbright
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=0
-#SBATCH --time=1-00:00
 
-module load python/3.10
-
-python parallel-solve.py $n $file $m 'True' $d $dv
-
-EOF
-    i=$((i + 1))
-    done
-        ;;
-    *)
-        echo "No solving mode selected, use -n by default"
-        echo "Simplifying $f for 10000 conflicts using CaDiCaL+CAS"
-        ./simplification/simplify-by-conflicts.sh constraints_${n}_${c} $n 10000
-
-        echo "Solving $f using MapleSAT+CAS"
-        ;;
-esac
 
