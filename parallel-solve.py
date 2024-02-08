a7f2948a00e5eead6943828a9c87cd1600745432
Brian Li <brianli.li@mail.utoronto.ca>
2024-02-08 16:01:09 -0500

    testing

--- parallel-solve.py
+++ parallel-solve.py
@@ -67,7 +67,7 @@ def worker(queue):
         queue.task_done()
 
 def cube(file_to_cube, m, order, numMCTS, queue, cutoff='d', cutoffv=5, d=0, n=0, v=0):
-    command = f"./cadical-ks/build/cadical-ks {file_to_cube} --order {order} -o {file_to_cube}.simp -e {file_to_cube}.ext -n -c 10000 | tee {file_to_cube}.simplog"
+    command = f"./simplification/simplify-by-conflicts.sh {file_to_cube} {order} 10000 | tee {file_to_cube}.simplog"
     # Run the command and capture the output
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
