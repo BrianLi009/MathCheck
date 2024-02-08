7fa8e49ec60994d83a113ec22032fa66c3139c6f
Brian Li <brianli@Brians-MacBook-Pro.local>
2024-02-07 22:15:45 -0500

    test

--- parallel-solve.py
+++ parallel-solve.py
@@ -86,7 +86,7 @@ def cube(file_to_cube, m, order, numMCTS, queue, cutoff='d', cutoffv=5, d=0, n=0
     command = f"sed -E 's/.* 0 [-]*([0-9]*) 0$/\\1/' < {file_to_cube}.ext | awk '$0<={mg}' | sort | uniq | wc -l"
 
     result = subprocess.run(command, shell=True, text=True, capture_output=True)
-    var_removed = v + int(result.stdout.strip())
+    var_removed = int(result.stdout.strip())
 
     print (f'{var_removed} variables removed from the cube')
 
