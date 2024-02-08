106d7831f182b3fa1a6350eac6dd86c0329dde4e
Brian Li <brianli@lawn-143-215-127-41.lawn.gatech.edu>
2024-02-08 11:41:59 -0500

    testiong

--- parallel-solve.py
+++ parallel-solve.py
@@ -95,8 +95,7 @@ def cube(file_to_cube, m, order, numMCTS, queue, cutoff='d', cutoffv=5, d=0, n=0
 
     print (f'{var_removed} variables removed from the cube')
 
-    subprocess.run(['rm', '-f', file_to_cube], check=True)
-    os.rename(f"{file_to_cube}.simp", rename_file(file_to_cube))
+    file_to_cube = f'{file_to_cube}.simp'
 
     if cutoff == 'd':
         if d >= cutoffv:
