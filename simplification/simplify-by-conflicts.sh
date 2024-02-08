5afa583b1615f982bc3a533e3f1e96e5e28fa83a
Brian Li <brianli.li@mail.utoronto.ca>
2024-02-08 16:45:20 -0500

    add extension stack

--- simplification/simplify-by-conflicts.sh
+++ simplification/simplify-by-conflicts.sh
@@ -23,45 +23,48 @@ o=$2 # Order
 m=$3 # Number of conflicts
 e=$((o*(o-1)/2)) # Number of edge variables
 
+# Create necessary directories
+mkdir -p log
+
 f_dir=$f
 
 # Simplify m seconds
 echo "simplifying for $m conflicts"
 i=1
-./cadical-ks/build/cadical-ks "$f_dir" "$f_dir.drat" --order $o --unembeddable-check 17 -o "$f_dir".simp1 -e "$f_dir".ext1 -n -c $m | tee "$f_dir".simp1
+./cadical-ks/build/cadical-ks "$f_dir" "$f_dir.drat" --order $o --unembeddable-check 17 -o simp/"$f_dir".simp1 -e simp/"$f_dir".ext1 -n -c $m | tee log/"$f_dir".simp1
 
 if [ "$s" != "true" ]; then
     echo "verifying the simplification now..."
-    ./drat-trim/drat-trim "$f_dir" "$f_dir.drat" -f | tee "$f_dir".simp1.verify
-    if ! grep -E "s DERIVATION|s VERIFIED" -q "$f_dir".simp1.verify; then
+    ./drat-trim/drat-trim "$f_dir" "$f_dir.drat" -f | tee log/"$f_dir".simp1.verify
+    if ! grep -E "s DERIVATION|s VERIFIED" -q log/"$f_dir".simp1.verify; then
         echo "ERROR: Proof not verified"
     fi
     rm -f "$f_dir.drat"
 fi
 
-conf_used=$(awk '/c conflicts:/ {print $3; exit}' "$f_dir".simp1)
+conf_used=$(awk '/c conflicts:/ {print $3; exit}' log/"$f_dir".simp1)
 echo "$conf_used conflicts used for simplification"
 
-if grep -q "UNSATISFIABLE" "$f_dir".simp1; then
+if grep -q "UNSATISFIABLE" log/"$f_dir".simp1; then
   conf_left=0
 else
   conf_left=$((m - conf_used))
 fi
 
 while (( conf_used < m && conf_left != 0 )); do
-  ./gen_cubes/concat-edge.sh $o "$f_dir".simp"$i" "$f_dir".ext"$i" | ./cadical-ks/build/cadical-ks /dev/stdin "$f_dir".simp"$i".drat --order $o --unembeddable-check 17 -o "$f_dir".simp$((i+1)) -e "$f_dir".ext$((i+1)) -n -c $conf_left | tee "$f_dir".simp$((i+1))
+  ./gen_cubes/concat-edge.sh $o simp/"$f_dir".simp"$i" simp/"$f_dir".ext"$i" | ./cadical-ks/build/cadical-ks /dev/stdin simp/"$f_dir".simp"$i".drat --order $o --unembeddable-check 17 -o simp/"$f_dir".simp$((i+1)) -e simp/"$f_dir".ext$((i+1)) -n -c $conf_left | tee log/"$f_dir".simp$((i+1))
   if [ "$s" != "true" ]; then
-    ./gen_cubes/concat-edge.sh $o "$f_dir".simp"$i" "$f_dir".ext"$i" | ./drat-trim/drat-trim /dev/stdin "$f_dir".simp"$i".drat -f | tee "$f_dir".simp$((i+1)).verify
-    if ! grep -E "s DERIVATION|s VERIFIED" -q "$f_dir".simp$((i+1)).verify; then
+    ./gen_cubes/concat-edge.sh $o simp/"$f_dir".simp"$i" simp/"$f_dir".ext"$i" | ./drat-trim/drat-trim /dev/stdin simp/"$f_dir".simp"$i".drat -f | tee log/"$f_dir".simp$((i+1)).verify
+    if ! grep -E "s DERIVATION|s VERIFIED" -q log/"$f_dir".simp$((i+1)).verify; then
       echo "ERROR: Proof not verified"
     fi
-    rm -f "$f_dir".simp"$i".drat "$f_dir".simp"$i"
+    rm -f simp/"$f_dir".simp"$i".drat simp/"$f_dir".simp"$i"
   fi
-  conf_used_2=$(awk '/c conflicts:/ {print $3; exit}' "$f_dir".simp$((i+1)))
+  conf_used_2=$(awk '/c conflicts:/ {print $3; exit}' log/"$f_dir".simp$((i+1)))
   conf_used=$((conf_used + conf_used_2))
   conf_left=$((m - conf_used))
   ((i++))
-  if grep -q "UNSATISFIABLE" "$f_dir".simp$i; then
+  if grep -q "UNSATISFIABLE" log/"$f_dir".simp$i; then
     break
   fi
 done
@@ -69,7 +72,6 @@ done
 echo "Called CaDiCaL $i times"
 
 # Output final simplified instance
-./gen_cubes/concat-edge.sh $o "$f_dir".simp"$i" "$f_dir".ext"$i" > "$f_dir".simp
+./gen_cubes/concat-edge.sh $o simp/"$f_dir".simp"$i" simp/"$f_dir".ext"$i" > "$f_dir".simp
 
-# Output final extension stack
 cat "$f_dir".ext* > "$f_dir".ext
