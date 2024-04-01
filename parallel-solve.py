85423be9e443bff295a20f076e8c19447aae1828
Brian Li <brianli@Brians-MacBook-Pro-2.local>
2024-04-01 11:24:16 -0400

    no need to remove drat during cubing since they are no longer generated

--- parallel-solve.py
+++ parallel-solve.py
@@ -87,7 +87,6 @@ def cube(original_file, cube, index, m, order, numMCTS, queue, cutoff='d', cutof
             print("the cube is UNSAT")
             if cube != "N":
                 os.remove(f'{cube}{index}.cnf')
-                os.remove(f'{cube}{index}.cnf.drat')
             os.remove(file_to_cube)
             os.remove(file_to_check)
             return
@@ -122,7 +121,6 @@ def cube(original_file, cube, index, m, order, numMCTS, queue, cutoff='d', cutof
         next_cube = f'{original_file}0'
     if cube != "N":
         os.remove(f'{cube}{index}.cnf')
-        os.remove(f'{cube}{index}.cnf.drat')
         os.remove(f'{file_to_cube}.temp')
     os.remove(file_to_cube)
     os.remove(file_to_check)
