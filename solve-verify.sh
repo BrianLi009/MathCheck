bf2e1f0a09feca6ef86b2d8ff402c79dc054e817
BrianLi009 <brianlizhengyu@gmail.com>
2025-02-03 22:38:53 -0500

    update solve-verify.sh

--- solve-verify.sh
+++ solve-verify.sh
@@ -1,34 +1,89 @@
 #!/bin/bash
 
-while getopts "s" opt
-do
+# Default values
+solver="maplesat"
+pseudo_check=true
+lex_order="smallest"
+orbit_val=""
+unembeddable=true
+skip_verify=false
+
+# Parse command line options
+while getopts "scpluo:h" opt; do
 	case $opt in
-        s) s="-s" ;;
-	esac
-done
-shift $((OPTIND-1))
-
-n=$1 #order
-f=$2 #instance file name
-
-[ "$1" = "-h" -o "$1" = "--help" -o "$#" -ne 2 ] && echo "
+		s) skip_verify=true ;;
+		c) solver="cadical" ;;
+		p) pseudo_check=false ;;
+		l) lex_order="greatest" ;;
+		o) orbit_val="$OPTARG" ;;
+		u) unembeddable=false ;;
+		h) echo "
 Description:
     Script for solving and generating drat proof for instance
 
 Usage:
-    ./solve-verify.sh n f e
+    ./solve-verify.sh [options] <n> <f>
 
-Options:
-    [-l]: generate learnt clauses
+Required Arguments:
     <n>: the order of the instance/number of vertices in the graph
     <f>: file name of the CNF instance to be solved
-" && exit
 
-#./cadical-ks/build/cadical-ks $f $f.drat --order $n --unembeddable-check 17 --perm-out $f.perm --proofsize 7168 | tee $f.log
-./maplesat-ks/simp/maplesat_static $f $f.drat -perm-out=$f.perm -exhaustive=$f.exhaust -order=$n -no-pre -minclause -max-proof-size=7168 -unembeddable-check=17 -unembeddable-out="$f.nonembed" | tee $f.log
+Options:
+    -s: Skip verification step
+    -c: Use CaDiCal solver (default: MapleSAT)
+    -p: Disable pseudo check
+    -l: Use lex-greatest ordering (default: lex-smallest)
+    -o VALUE: Set orbit value (required if -o is used)
+    -u: Disable unembeddable check (default: enabled)
+    -h: Show this help message
 
-if ! grep -q "UNSAT" "$f.log" || [ "$s" == "-s" ]; then
-    echo "instance not solved, no need to verify unless learnt clause or skipping verification"
+Example:
+    ./solve-verify.sh -c -l -o 42 17 instance.cnf
+" 
+			exit 0 ;;
+		:) echo "Option -$OPTARG requires an argument." >&2; exit 1 ;;
+		\?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+	esac
+done
+
+# Shift to get positional arguments
+shift $((OPTIND-1))
+
+# Check required arguments
+if [ $# -ne 2 ]; then
+	echo "Error: Required arguments missing. Use -h for help."
+	exit 1
+fi
+
+n=$1  # order
+f=$2  # instance file name
+
+# Construct solver command based on options
+if [ "$solver" = "cadical" ]; then
+	cmd="./cadical-ks/build/cadical-ks $f $f.drat --order $n"
+	[ "$unembeddable" = true ] && cmd="$cmd --unembeddable-check 13"
+	[ "$pseudo_check" = false ] && cmd="$cmd --no-pseudo-check"
+	[ "$lex_order" = "greatest" ] && cmd="$cmd --lex-greatest"
+	[ -n "$orbit_val" ] && cmd="$cmd --orbit $orbit_val"
+	cmd="$cmd --perm-out $f.perm"
 else
-    ./proof-module.sh $n $f $f.verify
+	cmd="./maplesat-ks/simp/maplesat_static $f $f.drat -order=$n -no-pre -minclause"
+	[ "$unembeddable" = true ] && cmd="$cmd -unembeddable-check=13"
+	[ "$pseudo_check" = false ] && cmd="$cmd -pseudo-test=false"
+	[ "$lex_order" = "greatest" ] && cmd="$cmd -lex-greatest"
+	[ -n "$orbit_val" ] && cmd="$cmd -orbit=$orbit_val"
+	cmd="$cmd -perm-out=$f.perm -exhaustive=$f.exhaust"
+fi
+
+# Print the command
+echo "Executing command: $cmd"
+
+# Execute solver command
+$cmd | tee $f.log
+
+# Verify unless skipped or not UNSAT
+if ! grep -q "UNSAT" "$f.log" || [ "$skip_verify" = true ]; then
+	echo "instance not solved, no need to verify unless skipping verification"
+else
+	./proof-module.sh $n $f $f.verify
 fi
