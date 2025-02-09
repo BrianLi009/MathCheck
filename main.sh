c174a9123e903ba65b513705161b838afc5945bc
BrianLi009 <brianlizhengyu@gmail.com>
2025-02-09 04:05:26 -0500

    translate -c and -C

--- main.sh
+++ main.sh
@@ -29,7 +29,7 @@ Options:
 
 Solver Options:
     --skip-verify, -S      Skip verification step
-    --cadical, -C          Use CaDiCal solver (default: MapleSAT)
+    --cadical, -C          Use CaDiCaL solver (default: MapleSAT)
     --no-pseudo, -P        Disable pseudo check
     --lex-greatest, -L     Use lex-greatest ordering (default: lex-smallest)
     --orbit=VALUE, -O=VALUE Set orbit value
@@ -180,8 +180,8 @@ edge_vars=$((order * (order-1) / 2))
 solver_opts_str=""
 # Skip verification (-s)
 [ "$solver_skip_verify" = true ] && solver_opts_str="$solver_opts_str -s"
-# Use CaDiCaL solver (-C)
-[ "$solver_use_cadical" = true ] && solver_opts_str="$solver_opts_str -C"
+# Use CaDiCaL solver (-C in main.sh, but needs to be -c for other scripts)
+[ "$solver_use_cadical" = true ] && solver_opts_str="$solver_opts_str -c"
 # Disable pseudo check (-p)
 [ "$solver_disable_pseudo" = true ] && solver_opts_str="$solver_opts_str -p"
 # Use lex-greatest ordering (-l)
