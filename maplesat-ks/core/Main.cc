c3070240f7067756c1e412c4408983dee3af1b8e
BrianLi009 <brianlizhengyu@gmail.com>
2025-02-10 18:20:29 -0500

    printing

--- maplesat-ks/core/Main.cc
+++ maplesat-ks/core/Main.cc
@@ -84,6 +84,11 @@ int main(int argc, char** argv)
         IntOption    verb   ("MAIN", "verb",   "Verbosity level (0=silent, 1=some, 2=more).", 1, IntRange(0, 2));
         IntOption    cpu_lim("MAIN", "cpu-lim","Limit on CPU time allowed in seconds.\n", INT32_MAX, IntRange(0, INT32_MAX));
         IntOption    mem_lim("MAIN", "mem-lim","Limit on memory usage in megabytes.\n", INT32_MAX, IntRange(0, INT32_MAX));
+        IntOption    order  ("MAIN", "order",  "Order of graph.\n", 0, IntRange(0, INT32_MAX));
+        
+        parseOptions(argc, argv, true);
+
+        // Now declare orbit_opt with order as default
         IntOption    orbit_opt("MAIN", "orbit", "Enable orbit pruning for graphs with order >= N (default: order of graph)", order, IntRange(0, INT32_MAX));
         
         parseOptions(argc, argv, true);
